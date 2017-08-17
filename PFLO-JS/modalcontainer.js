329395397b2f784a88be858b5cc399b2ba5034bd
ThinhBui94 <thinhducbui94@gmail.com>
2017-08-17 17:00:51 +0700

    PFLO

--- PFLO-JS/modalcontainer.js
+++ PFLO-JS/modalcontainer.js
@@ -1,4 +1,29 @@
 $(function(){
+	function check1024(){
+		if(window.matchMedia("(max-width:1024px)").matches){
+			$(".item5").html('<i class="material-icons">contact_mail</i>');
+			$(".item4").html('<i class="material-icons">face</i>');
+			$(".item2").html('<i class="material-icons">event_note</i>');
+			$(".item1").html('<i class="material-icons">home</i>');
+		} else{
+			$(".item5").html('Contact');
+			$(".item4").html('AboutMe');
+			$(".item2").html('Project');
+			$(".item1").html('ThinhBui94');
+		}
+	}
+
+	function check700(){
+		if(window.matchMedia("(max-width:700px)").matches){
+			$(".item1, .item2, .item4, .item5").css("display", "none");
+		} else{
+			$(".item1, .item2, .item4, .item5").css("display", "flex");
+		}
+	}
+
+	$(window).on('resize', check1024);
+	$(window).on('resize', check700);
+
 	$("#main-Container").css("display", "none");
 
 	$("#enter").mouseenter(function(){
@@ -22,12 +47,19 @@ $(function(){
 		setTimeout(function(){
 			$("#modal-Container").css("display", "none");
 			$(".modal-Enter").css("display", "none");
-			$("#main-Container").css("display", "grid");
+			$("#main-Container").css("display", "block");
 			$("#main-Container").addClass("animated bounceInLeft");
 		}, 600);
+		check1024();
+		check700();
 	})
 
 	$("#leave").on("click", function(){
 		window.location = "https://www.google.com";
 	})
+
+	$("#myButton").on("click", function(){
+		$("#myButton").toggleClass("change");
+	})
+	
 })
